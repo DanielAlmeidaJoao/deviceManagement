961ea1a023b47ef4eb4d804f4672444655eb4bed
Daniel João <daniel.joao@cultofbits.com>
2025-03-02 19:26:26 +0000

    Configs jpaRepository

--- deviceManagement/src/main/java/com/springLessons/deviceManagement/repositories/DevicesRepository.java
+++ deviceManagement/src/main/java/com/springLessons/deviceManagement/repositories/DevicesRepository.java
@@ -1,9 +1,9 @@
 package com.springLessons.deviceManagement.repositories;
 
-import com.springLessons.taskManagement.dtos.Task;
+import com.springLessons.deviceManagement.databaseEntities.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface DevicesRepository extends JpaRepository<Device, Long> {
 }
