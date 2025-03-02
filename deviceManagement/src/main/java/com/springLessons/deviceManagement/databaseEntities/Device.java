43e56a12c4190a63afca17c0374e297c842cf998
Daniel João <daniel.joao@cultofbits.com>
2025-03-02 19:24:49 +0000

    Adds Device entity

---
+++ deviceManagement/src/main/java/com/springLessons/deviceManagement/databaseEntities/Device.java
@@ -0,0 +1,71 @@
+package com.springLessons.deviceManagement.databaseEntities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Devices")
+public class Device {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-increment ID
+    private Long id;
+
+    private String name;
+    private String brand;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private State state;
+    private long creationTime;
+
+    public Device(String name, String brand, State state, long creationTime) {
+        this.name = name;
+        this.brand = brand;
+        this.state = state;
+        this.creationTime = creationTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public long getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(long creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    // Constructors
+    public Device() {}
+
+}
---
+++ deviceManagement/src/main/java/com/springLessons/deviceManagement/databaseEntities/State.java
@@ -0,0 +1,5 @@
+package com.springLessons.deviceManagement.databaseEntities;
+
+public enum State {
+    AVAILABLE, IN_USE, INACTIVE
+}
