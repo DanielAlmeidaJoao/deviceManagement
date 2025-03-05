7c71735622c80a6990bf70416932be785fee7320
Daniel João <daniel.joao@cultofbits.com>
2025-03-05 01:19:04 +0000

    Updates the values of the states

--- deviceManagement/src/main/java/com/springLessons/deviceManagement/databaseEntities/State.java
+++ deviceManagement/src/main/java/com/springLessons/deviceManagement/databaseEntities/State.java
@@ -1,5 +1,11 @@
 package com.springLessons.deviceManagement.databaseEntities;
 
 public enum State {
-    AVAILABLE, IN_USE, INACTIVE
+    AVAILABLE("available"), IN_USE("in-use"), INACTIVE("inactive");
+
+    public final String label;
+
+    private State(String label) {
+        this.label = label;
+    }
 }
